9a3fec7bd8088c5bf2a4833567be4cd3d496919e
febridev <febriyantabidin@gmail.com>
2022-11-29 23:20:56 +0700

    error user register

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, app, request,jsonify
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
+from werkzeug.security import check_password_hash,generate_password_hash
+import validators
+from src.database import Tuser, db
+
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
 
 @auth.post('/register')
 def register():
-    return "User Created"
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+
+    # validation
+    if len(password) < 6:
+        return jsonify({'error':"Password Too Short"}), HTTP_400_BAD_REQUEST
+
+    if len(username) < 3:
+        return jsonify({'error':"Username Too Short"}), HTTP_400_BAD_REQUEST
+    
+    if not username.isalnum() or " " in username:
+        return jsonify({'error':"Username should be alphanumeric, also no space"}), HTTP_400_BAD_REQUEST
+    
+    if not validators.email(email):
+        return jsonify({'error':"Email is not valid"}), HTTP_400_BAD_REQUEST
+    
+    if Tuser.query.filter_by(email=email).first() is not None:
+        return jsonify({'error':"Email is taken"}), HTTP_409_CONFLICT
+    
+    if Tuser.query.filter_by(username=username).first() is not None:
+        return jsonify({'error':"Username is taken"}), HTTP_409_CONFLICT
+    
+    #hash password
+    pwd_hash = generate_password_hash(password)
+    
+    # insert 
+    tuser = Tuser(username=username, email=email, password=pwd_hash)
+    # db.connection()
+    db.session.add(tuser)
+    db.session.commit
+
+    return jsonify({
+        'message':"User Created",
+        'user': {
+            'username': username, 'email':email
+        }
+    }), HTTP_201_CREATED
+
+    # return "User Created"
 
 @auth.get('/me')
 def me():
--- src/database.py
+++ src/database.py
@@ -5,26 +5,28 @@ import random
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Tuser(db.Model):
+    __tablename__ = 'tuser'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
-    bookmarks = db.relationship('Bookmark', backref="user")
+    bookmarks = db.relationship('Bookmark', backref="tuser")
 
     def __repr__(self) -> str:
-        return 'User>>> {self.username}'
+        return 'Tuser>>> {self.username}'
 
 
 class Bookmark(db.Model):
+    __tablename__ = 'bookmark'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text, nullable=True)
     url = db.Column(db.Text, nullable=False)
     short_url = db.Column(db.String(3), nullable=True)
     visits = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('tuser.id'))
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
@@ -45,4 +47,4 @@ class Bookmark(db.Model):
         self.short_url = self.generate_short_characters()
 
     def __repr__(self) -> str:
-        return 'Boomark>>> {self.url}'
+        return 'Bookmark>>> {self.url}'
